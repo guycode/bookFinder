2e179ca952394abacb2729ce6abf9647e4b1ca73
Rich Lane <richlanedesign@gmail.com>
2019-03-17 21:28:14 -0600

    Updates

---
+++ precache-manifest.190fa118daad58ec7a391fa4d2dfeebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bookFinder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e1756705e8c4a8b5a98b",
+    "url": "/bookFinder/static/js/runtime~main.6d508418.js"
+  },
+  {
+    "revision": "d7417b312b3f7ca598e9",
+    "url": "/bookFinder/static/js/main.69ad0b1e.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/bookFinder/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "d7417b312b3f7ca598e9",
+    "url": "/bookFinder/static/css/main.417b8bba.chunk.css"
+  },
+  {
+    "revision": "eb08a1bcfd3566e8e55d289a67d5fe10",
+    "url": "/bookFinder/index.html"
+  }
+];
