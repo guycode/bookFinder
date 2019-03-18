b0fbdde4318aa70c0ad673bd4cf9b660d34d3bed
Rich Lane <richlanedesign@gmail.com>
2019-03-17 22:54:51 -0600

    not finished quite yet

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/bookfinder", { useNewUrlParser: true })
+  .then(() => {
+    console.log("🗄 ==> Successfully connected to mongoDB.");
+  })
+  .catch((err) => {
+    console.log(`Error connecting to mongoDB: ${err}`);
+  });
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './util.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Gallery from './components/Library.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      items: []
+    };
+  }
+
+  search() {
+    const API_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+    fetch(`${API_URL}${this.state.query}`)
+      .then(response => response.json())
+      .then(json => {
+        let {items} = json;
+        this.setState({items})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Book Finder</h1>
         </header>
+        <div className="container main-content">
+          <FormGroup>
+            <InputGroup>
+              <FormControl type="text" placeholder="Search for a book"
+              onChange={ event => this.setState({ query: event.target.value }) }
+              onKeyPress={ event => {
+                if ('Enter' === event.key) {
+                  this.search();
+                }
+              }} />
+              <InputGroup.Addon onClick={() => this.search()}>
+                <Glyphicon glyph="search"></Glyphicon>
+              </InputGroup.Addon>
+            </InputGroup>
+          </FormGroup>
+          <Gallery items={this.state.items} />
+        </div>
       </div>
     );
   }
